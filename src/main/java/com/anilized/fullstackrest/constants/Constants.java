5a7b2f8cde964422f822203c51635b388de70e26
anilized <45407535+anilized@users.noreply.github.com>
2021-11-06 03:40:07 +0300

    deleted client_id and access token

--- src/main/java/com/anilized/fullstackrest/constants/Constants.java
+++ src/main/java/com/anilized/fullstackrest/constants/Constants.java
@@ -3,8 +3,8 @@ package com.anilized.fullstackrest.constants;
 
 public final class Constants {
 	
-	public static final String CLIENT_ID= "mmvbtaa7q1mv5gx8yjma7hh9vopzbr";
-	public static final String ACCESS_TOKEN = "Bearer 7yuvc34nkmj7a0771qs5ylld9ev1ak";
+	public static final String CLIENT_ID= "YOUR_CLIENT_ID";
+	public static final String ACCESS_TOKEN = "Bearer YOUR_ACCESS_TOKEN";
 	public static final String ENDPOINT = "https://api.igdb.com/v4/";
 
 }
